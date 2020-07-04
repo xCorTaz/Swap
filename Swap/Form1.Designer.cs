177ffb5b69e25d05009a9e3ba5326ea821fa751b
CorTaz <65075034+xCorTaz@users.noreply.github.com>
2020-07-05 02:56:27 +0400

    Add files via upload

---
+++ Swap/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Swap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Userbox = new System.Windows.Forms.TextBox();
+            this.Passwordbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.threadbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Targetbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.attemptsbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Userbox
+            // 
+            this.Userbox.Location = new System.Drawing.Point(97, 80);
+            this.Userbox.Name = "Userbox";
+            this.Userbox.Size = new System.Drawing.Size(206, 20);
+            this.Userbox.TabIndex = 0;
+            // 
+            // Passwordbox
+            // 
+            this.Passwordbox.Location = new System.Drawing.Point(97, 127);
+            this.Passwordbox.Name = "Passwordbox";
+            this.Passwordbox.Size = new System.Drawing.Size(206, 20);
+            this.Passwordbox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(0, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(0, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Location = new System.Drawing.Point(323, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // threadbox
+            // 
+            this.threadbox.Location = new System.Drawing.Point(259, 50);
+            this.threadbox.Name = "threadbox";
+            this.threadbox.Size = new System.Drawing.Size(44, 20);
+            this.threadbox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(174, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Threads:";
+            // 
+            // Targetbox
+            // 
+            this.Targetbox.Location = new System.Drawing.Point(97, 177);
+            this.Targetbox.Name = "Targetbox";
+            this.Targetbox.Size = new System.Drawing.Size(206, 20);
+            this.Targetbox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(23, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Target:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(256, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Attempts:";
+            // 
+            // attemptsbox
+            // 
+            this.attemptsbox.Location = new System.Drawing.Point(334, 17);
+            this.attemptsbox.Name = "attemptsbox";
+            this.attemptsbox.Size = new System.Drawing.Size(36, 20);
+            this.attemptsbox.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 220);
+            this.Controls.Add(this.attemptsbox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Targetbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.threadbox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Passwordbox);
+            this.Controls.Add(this.Userbox);
+            this.Name = "Form1";
+            this.Text = "Swapper";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Userbox;
+        private System.Windows.Forms.TextBox Passwordbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox threadbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Targetbox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox attemptsbox;
+    }
+}
+
---
+++ Swap/Form1.cs
@@ -0,0 +1,331 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Swap
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public string uuID;
+        public bool running;
+        public static bool TellMe = false;
+        public bool GetSettingsBool;
+        public bool SwitchLoginBool;
+        public CookieContainer Sessionz;
+        private bool i;
+        private int x;
+        private int y;
+        public string accSettings;
+        public bool IfLoggedIn;
+        public static string AccSettings2;
+        public bool TurboGO;
+        public int attempts;
+        public static CookieContainer CookiesX = new CookieContainer();
+        private IContainer component;
+        internal delegate void anonRedir();
+        public static string getaccountsettings(CookieContainer ssn)
+        {
+            string text = string.Empty;
+            string text2 = string.Empty;
+            string text3 = string.Empty;
+            string text4 = string.Empty;
+            string text5 = string.Empty;
+            string text6 = string.Empty;
+            string text7 = string.Empty;
+            string text8 = string.Empty;
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("https://i.instagram.com/api/v1/accounts/current_user/?edit=true");
+            string str = Guid.NewGuid().ToString().ToUpper();
+            httpWebRequest.Host = "i.instagram.com";
+            httpWebRequest.UserAgent = "Instagram 9.4.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)";
+            httpWebRequest.Headers.Add("Accept-Language", "ar;q=1, en;q=0.9");
+            httpWebRequest.KeepAlive = true;
+            httpWebRequest.Proxy = null;
+            httpWebRequest.ContentType = "multipart/form-data; boundary=" + str;
+            httpWebRequest.CookieContainer = ssn;
+            using (StreamReader streamReader = new StreamReader(httpWebRequest.GetResponse().GetResponseStream()))
+            {
+                string input = streamReader.ReadToEnd();
+                text = Regex.Match(input, "pk\": (.*?),").Value;
+                bool flag = Operators.CompareString(text, null, false) == 0;
+                if (flag)
+                {
+                    text = Regex.Match(input, "pk\": (.*?)}").Value;
+                    bool flag2 = Operators.CompareString(text, null, false) == 0;
+                    if (flag2)
+                        return "|||||||";
+                }
+                text2 = Regex.Match(input, "full_name\": \"(.*?)\"").Value;
+                text3 = Regex.Match(input, "is_private\": (.*?),").Value;
+                bool flag3 = Operators.CompareString(text3, null, false) == 0;
+                if (flag3)
+                {
+                    text3 = Regex.Match(input, "is_private\": (.*?)}").Value;
+                    bool flag4 = Operators.CompareString(text3, null, false) == 0;
+                    if (flag4)
+                        return "|||||||";
+                }
+                text4 = Regex.Match(input, "phone_number\": \"(.*?)\"").Value;
+                text5 = Regex.Match(input, "biography\": \"(.*?)\"").Value;
+                text6 = Regex.Match(input, "gender\": (.*?),").Value;
+                bool flag5 = Operators.CompareString(text6, null, false) == 0;
+                if (flag5)
+                {
+                    text6 = Regex.Match(input, "gender\": (.*?)}").Value;
+                    bool flag6 = Operators.CompareString(text6, null, false) == 0;
+                    if (flag6)
+                        return "|||||||";
+                }
+                text7 = Regex.Match(input, "email\": \"(.*?)\"").Value;
+                text8 = Regex.Match(input, "external_url\": \"(.*?)\"").Value;
+                Form1.AccSettings2 = string.Format("{0}|{1}|{2}|{3}|{4}|{5}|{6}|{7}", new object[] { text, text2, text3, text4, text5, text6, text7, text8 });
+            }
+            return Form1.AccSettings2;
+        }
+        public static CookieContainer igLogin(string Username, string Password, string uuID)
+        {
+            try
+            {
+                string str = Guid.NewGuid().ToString().ToUpper();
+                StringBuilder stringBuilder = new StringBuilder();
+                StringBuilder stringBuilder2 = stringBuilder;
+                string text = string.Concat(new string[] { "{\"_uuid\":\"", uuID, "\",\"password\":\"", Password, "\",\"username\":\"", Username, "\",\"device_id\":\"", uuID, "\",\"from_reg\":false,\"_csrftoken\":\"missing\",\"login_attempt_count\":0}" });
+                text = string.Format("{0}.{1}", Form1.Sha256_hash(text, "b03e0daaf2ab17cda2a569cace938d639d1288a1197f9ecf97efd0a4ec0874d7"), text);
+                stringBuilder2.AppendLine("--" + str);
+                stringBuilder2.AppendLine("Content-Disposition: form-data; name=\"signed_body\"");
+                stringBuilder2.AppendLine();
+                stringBuilder2.AppendLine(text);
+                stringBuilder2.AppendLine("--" + str);
+                stringBuilder2.AppendLine("Content-Disposition: form-data; name=\"ig_sig_key_version\"");
+                stringBuilder2.AppendLine();
+                stringBuilder2.AppendLine("4");
+                stringBuilder2.Append("--" + str + "--");
+                byte[] bytes = Encoding.UTF8.GetBytes(stringBuilder.ToString());
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("https://i.instagram.com/api/v1/accounts/login/");
+                httpWebRequest.AutomaticDecompression = DecompressionMethods.GZip;
+                httpWebRequest.Method = "POST";
+                httpWebRequest.Host = "i.instagram.com";
+                httpWebRequest.UserAgent = "Instagram 9.4.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)";
+                httpWebRequest.Headers.Add("Accept-Language", "ar;q=1, en;q=0.9");
+                httpWebRequest.KeepAlive = true;
+                httpWebRequest.Proxy = null;
+                httpWebRequest.ContentType = "multipart/form-data; boundary=" + str;
+                httpWebRequest.ContentLength = System.Convert.ToInt64(bytes.Length);
+                httpWebRequest.CookieContainer = Form1.CookiesX;
+                Stream requestStream = httpWebRequest.GetRequestStream();
+                requestStream.Write(bytes, 0, bytes.Length);
+                requestStream.Close();
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream());
+                bool flag = streamReader.ReadToEnd().Contains("{\"logged_in_user");
+                if (flag)
+                    return Form1.CookiesX;
+                streamReader.Close();
+                httpWebResponse.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+            return null/* TODO Change to default(_) if this is not a reference type */;
+        }
+        public void redirLogin()
+        {
+
+        }
+        private void StatusButton(Button btn, string input)
+        {
+            btn.Invoke(new Form1.anonRedir(() =>
+            {
+                btn.Text = string.Format(input, new object[0] { });
+            }));
+        }
+        public Form1()
+        {
+            TellMe = false;
+            CookiesX = new CookieContainer();
+            running = false;
+            TurboGO = false;
+            attempts = 0;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            uuID = Guid.NewGuid().ToString().ToUpper();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool flag = string.IsNullOrWhiteSpace(this.Userbox.Text);
+            if (flag)
+                MessageBox.Show("Username is empty!");
+            else
+            {
+                bool flag2 = string.IsNullOrWhiteSpace(this.Passwordbox.Text);
+                if (flag2)
+                    MessageBox.Show("Password is empty!");
+                else
+                {
+                    bool flag3 = this.running;
+                    if (flag3)
+                        this.StatusButton(button1, "start");
+                    else
+                    {
+                        Thread thread = new Thread(this.redirLogin);
+                        thread.Start();
+                    }
+                }
+            }
+        }
+        public static string Sha256_hash(string value, string salt)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            HMACSHA256 hmacsha = new HMACSHA256(Encoding.UTF8.GetBytes(salt));
+            hmacsha.ComputeHash(Encoding.UTF8.GetBytes(value));
+            byte[] hash = hmacsha.Hash;
+            int num = hash.Length - 1;
+            int i = 0;
+            int num2;
+            /*while(i <= num2)
+            {
+                stringBuilder.Append(hash[i].ToString("x2"));
+                i += 1;
+                num2 = num;
+            }*/
+            return stringBuilder.ToString();
+        }
+        public object swap(string Username)
+        {
+            object result = false;
+            try
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                StringBuilder stringBuilder2 = stringBuilder;
+                string[] array = this.accSettings.Split(new char[] { '|' });
+                string text = string.Concat(new string[] { "{\"_uid\":\"", array[0], "\",\"_csrftoken\":\"missing\",\"first_name\":\"Moved By @ak.p5(Cortaz)", "", "\",\"is_private\":\"", array[2], "\",\"phone_number\":\"", array[3], "\",\"biography\":\"", array[4], "\",\"username\":\"", Username, "\",\"gender\":\"", array[5], "\",\"email\":\"", array[6], "\",\"_uuid\":\"", this.uuID, "\",\"external_url\":\"", array[7], "\"}" });
+                text = string.Format("{0}.{1}", Form1.Sha256_hash(text, "673581b0ddb792bf47da5f9ca816b613d7996f342723aa06993a3f0552311c7d"), text);
+                stringBuilder2.AppendLine("signed_body=" + WebUtility.UrlEncode(text) + "&ig_sig_key_version=5");
+                byte[] bytes = Encoding.UTF8.GetBytes(stringBuilder.ToString());
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("https://i.instagram.com/api/v1/accounts/edit_profile/");
+                httpWebRequest.AutomaticDecompression = DecompressionMethods.GZip;
+                httpWebRequest.Method = "POST";
+                httpWebRequest.Host = "i.instagram.com";
+                httpWebRequest.UserAgent = "Instagram 9.4.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US";
+                httpWebRequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+                httpWebRequest.Headers.Add("Accept-Language", "ar;q=1, en;q=0.9");
+                httpWebRequest.KeepAlive = true;
+                httpWebRequest.ContentLength = System.Convert.ToInt64(bytes.Length);
+                httpWebRequest.CookieContainer = this.Sessionz;
+                Stream requestStream = httpWebRequest.GetRequestStream();
+                requestStream.Write(bytes, 0, bytes.Length);
+                requestStream.Close();
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream());
+                bool flag = streamReader.ReadToEnd().Contains("\"status\": \"ok\"");
+                if (flag)
+                    result = true;
+                streamReader.ReadToEnd();
+                streamReader.Close();
+                httpWebResponse.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+            return result;
+        }
+        public void swapYuh()
+        {
+            // The following expression was wrapped in a checked-statement
+            while (this.running)
+            {
+                bool flag = Conversions.ToBoolean(RuntimeHelpers.GetObjectValue(this.swap(this.Targetbox.Text)));
+                if (flag)
+                {
+                    MessageBox.Show("- Moved : @" + this.Targetbox.Text, "-@ak.p5, @1337abood", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    this.running = false;
+                }
+                else
+                {
+                    int num = this.attempts;
+                    this.attempts = num + 1;
+                    this.attemptsbox.Text = Conversions.ToString(this.attempts);
+                }
+                Thread.Sleep(System.Convert.ToInt32(Math.Round(Conversions.ToDouble(this.threadbox.Text))));
+            }
+        }
+        public void SwapYuh2()
+        {
+            while (this.running)
+            {
+                bool flag = Conversions.ToBoolean(RuntimeHelpers.GetObjectValue(this.swap(this.threadbox.Text)));
+                if (flag)
+                {
+                    MessageBox.Show("- Moved : @" + this.Targetbox.Text, -"@ak.p5, @1337abood", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    this.running = false;
+                }
+                else
+                {
+                    int num = this.attempts;
+                    this.attempts = num + 1;
+                    this.attemptsbox.Text = Conversions.ToString(this.attempts);
+                }
+                Thread.Sleep(System.Convert.ToInt32(Math.Round(Conversions.ToDouble(this.threadbox.Text))));
+            }
+        }
+        public void swapYuh3()
+        {
+            while (this.running)
+            {
+                bool flag = Conversions.ToBoolean(RuntimeHelpers.GetObjectValue(this.swap(this.Targetbox.Text)));
+                if (flag)
+                {
+                    MessageBox.Show("- Moved : @" + this.Targetbox.Text, "-@ak.p5, @1337abood", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    this.running = false;
+                }
+                else
+                {
+                    int num = this.attempts;
+                    this.attempts = num + 1;
+                    this.attemptsbox.Text = Conversions.ToString(this.attempts);
+                }
+                Thread.Sleep(System.Convert.ToInt32(Math.Round(Conversions.ToDouble(this.threadbox.Text))));
+            }
+        }
+
+        private void SwitchButton(Button Btn, bool input)
+        {
+            Btn.Invoke(new Form1.anonRedir(() =>
+            {
+                Btn.Enabled = input;
+            }));
+        }
+
+        private void SwitchCheckBox(CheckBox Chk, bool input)
+        {
+            Chk.Invoke(new Form1.anonRedir(() =>
+            {
+                Chk.Enabled = input;
+            }));
+        }
+
+        private void SwitchTextz(TextBox txt, bool input)
+        {
+            txt.Invoke(new Form1.anonRedir(() =>
+            {
+                txt.Enabled = input;
+            }));
+        }
+    }
+}
